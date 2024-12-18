9815b926378bd4b012b8a084364c102f668554fc
sepehr-safari <safari.sepehr@gmail.com>
2024-12-18 19:19:24 +0330

    feat: update role setup component to use string literals for user roles

--- src/features/setup-role/index.tsx
+++ src/features/setup-role/index.tsx
@@ -1,19 +1,18 @@
 import { Button } from '@/shared/components/ui/button';
 
 import { useUpdateUserRole } from '@/shared/hooks';
-import { UserRole } from '@/shared/types';
 
 export const SetupRole = () => {
   const { updateRole } = useUpdateUserRole();
 
   return (
-    <div className="flex flex-col items-center gap-2 m-2">
+    <div className="p-4 flex flex-col items-center gap-2">
       <h4>Do you need help or want to provide help?</h4>
 
       <div className="flex items-center gap-4">
-        <Button onClick={() => updateRole(UserRole.Peh)}>Need Help</Button>
+        <Button onClick={() => updateRole('peh')}>Need Help</Button>
 
-        <Button onClick={() => updateRole(UserRole.Agency)}>Want to Help</Button>
+        <Button onClick={() => updateRole('agency')}>Want to Help</Button>
       </div>
     </div>
   );
