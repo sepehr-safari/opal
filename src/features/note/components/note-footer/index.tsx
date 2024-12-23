c3adbe2c9eedba2e79c2c7456757002ed9556a34
sepehr-safari <safari.sepehr@gmail.com>
2024-12-23 18:00:07 +0330

    feat: integrate NoteLikeBtn, NoteRepostBtn, and NoteZapBtn into NoteFooter component

--- src/features/note/components/note-footer/index.tsx
+++ src/features/note/components/note-footer/index.tsx
@@ -1,29 +1,25 @@
 import { NDKEvent } from '@nostr-dev-kit/ndk';
-import { BookmarkIcon, HeartIcon, MessageSquareIcon, Repeat2Icon, ZapIcon } from 'lucide-react';
+import { BookmarkIcon, MessageSquareIcon } from 'lucide-react';
 
 import { Button } from '@/shared/components/ui/button';
 
-export const NoteFooter = ({}: { event: NDKEvent }) => {
+import { NoteLikeBtn, NoteRepostBtn, NoteZapBtn } from '../';
+
+export const NoteFooter = ({ event }: { event: NDKEvent }) => {
   return (
     <div className="">
       <div className="flex items-center justify-between gap-2">
-        <Button variant="link" size="icon" className="opacity-40 hover:opacity-100">
+        <Button variant="link" size="icon" className="opacity-50 hover:opacity-100">
           <MessageSquareIcon size={18} />
         </Button>
 
-        <Button variant="link" size="icon" className="opacity-40 hover:opacity-100">
-          <ZapIcon size={18} />
-        </Button>
+        <NoteZapBtn event={event} />
 
-        <Button variant="link" size="icon" className="opacity-40 hover:opacity-100">
-          <HeartIcon size={18} />
-        </Button>
+        <NoteLikeBtn event={event} />
 
-        <Button variant="link" size="icon" className="opacity-40 hover:opacity-100">
-          <Repeat2Icon size={18} />
-        </Button>
+        <NoteRepostBtn event={event} />
 
-        <Button variant="link" size="icon" className="opacity-40 hover:opacity-100">
+        <Button variant="link" size="icon" className="opacity-50 hover:opacity-100">
           <BookmarkIcon size={18} />
         </Button>
       </div>
