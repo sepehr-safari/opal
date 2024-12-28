f815545269ef6a2c6ed0054c7ac57a4dee6edce6
sepehr-safari <safari.sepehr@gmail.com>
2024-12-28 13:32:35 +0330

    refactor: clean up imports and remove unused hooks in HousingRequestListItem

--- src/features/housing-request-list/components/housing-request-list-item/index.tsx
+++ src/features/housing-request-list/components/housing-request-list-item/index.tsx
@@ -1,15 +1,19 @@
+import { NDKUser } from '@nostr-dev-kit/ndk';
+import { useRealtimeProfile } from 'nostr-hooks';
+import { useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { Button } from '@/shared/components/ui/button';
 
 import { Spinner } from '@/shared/components/spinner';
 
-import { useRealtimeProfile } from '@/shared/hooks';
+// import { useManageHousingRequest } from '@/shared/hooks/use-manage-housing-request';
 import { HousingRequest } from '@/shared/types';
-import { NDKUser } from '@nostr-dev-kit/ndk';
-import { useMemo } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export const HousingRequestListItem = ({ housingRequest }: { housingRequest: HousingRequest }) => {
   const navigate = useNavigate();
+
+  // const { approveHousingRequest, rejectHousingRequest } = useManageHousingRequest();
 
   const npub = useMemo(
     () => new NDKUser({ pubkey: housingRequest.pehPubkey }).npub,
