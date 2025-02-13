d3c43a5c05252992aaeda8c7b92eb6a72e4aeb6f
sepehr-safari <safari.sepehr@gmail.com>
2025-02-13 16:18:06 +0330

    add confirmation dialog for credentials on logout

--- src/features/active-user-widget/index.tsx
+++ src/features/active-user-widget/index.tsx
@@ -2,9 +2,19 @@ import { pdf } from '@react-pdf/renderer';
 import { saveAs } from 'file-saver';
 import { KeySquareIcon, PowerIcon, UserIcon } from 'lucide-react';
 import { useActiveUser, useLogin, useRealtimeProfile } from 'nostr-hooks';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/shared/components/ui/alert-dialog';
 import { Avatar, AvatarFallback, AvatarImage } from '@/shared/components/ui/avatar';
 import {
   DropdownMenu,
@@ -19,6 +29,8 @@ import { ellipsis } from '@/shared/utils';
 import { CredentialsDocument } from '@/features/credentials-document';
 
 export const ActiveUserWidget = () => {
+  const [open, setOpen] = useState(false);
+
   const { activeUser } = useActiveUser();
   const { profile } = useRealtimeProfile(activeUser?.pubkey);
   const { logout, loginData } = useLogin();
@@ -39,46 +51,78 @@ export const ActiveUserWidget = () => {
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <div className="flex items-center gap-2 cursor-pointer bg-secondary rounded-full lg:pl-1 lg:pr-2 lg:py-1">
-          <Avatar>
-            <AvatarImage
-              src={profile?.image?.toString()}
-              alt={profile?.name?.toString()}
-              className="object-cover"
-            />
-            <AvatarFallback className="bg-background/50" />
-          </Avatar>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="flex items-center gap-2 cursor-pointer bg-secondary rounded-full lg:pl-1 lg:pr-2 lg:py-1">
+            <Avatar>
+              <AvatarImage
+                src={profile?.image?.toString()}
+                alt={profile?.name?.toString()}
+                className="object-cover"
+              />
+              <AvatarFallback className="bg-background/50" />
+            </Avatar>
 
-          <div className="text-start pr-2 hidden lg:block">
-            {profile?.name && <div className="text-sm">{profile.name.toString()}</div>}
-            <div className="text-xs text-primary/70">
-              {profile?.nip05?.toString() || ellipsis(activeUser.npub, 10)}
+            <div className="text-start pr-2 hidden lg:block">
+              {profile?.name && <div className="text-sm">{profile.name.toString()}</div>}
+              <div className="text-xs text-primary/70">
+                {profile?.nip05?.toString() || ellipsis(activeUser.npub, 10)}
+              </div>
             </div>
           </div>
-        </div>
-      </DropdownMenuTrigger>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" sideOffset={8}>
-        <DropdownMenuItem onClick={() => navigate(`/profile/${activeUser.npub}`)}>
-          <UserIcon className="w-4 h-4 mr-2" />
-          Profile
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() =>
-            downloadCredentials({ npub: activeUser.npub, nsec: loginData.privateKey || '' })
-          }
-        >
-          <KeySquareIcon className="w-4 h-4 mr-2" />
-          Credentials (PDF)
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={logout}>
-          <PowerIcon className="w-4 h-4 mr-2" />
-          Logout
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        <DropdownMenuContent align="end" sideOffset={8}>
+          <DropdownMenuItem onClick={() => navigate(`/profile/${activeUser.npub}`)}>
+            <UserIcon className="w-4 h-4 mr-2" />
+            Profile
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() =>
+              downloadCredentials({ npub: activeUser.npub, nsec: loginData.privateKey || '' })
+            }
+          >
+            <KeySquareIcon className="w-4 h-4 mr-2" />
+            Credentials (PDF)
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <PowerIcon className="w-4 h-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Do you want to save your credentials?</AlertDialogTitle>
+            <AlertDialogDescription>
+              If you logout without saving your credentials, you will lose access to your account.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              onClick={() => {
+                logout();
+              }}
+            >
+              No! Logout without saving
+            </AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() =>
+                downloadCredentials({
+                  npub: activeUser.npub,
+                  nsec: loginData.privateKey || '',
+                }).then(() => logout())
+              }
+            >
+              Yes! Save and Logout
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
